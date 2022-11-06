9e88899f219eb534ff12bcd5253c1a49eb47b50f
강명구 <mg5566@hanmail.net>
2022-11-06 15:31:57 +0900

    feat: add game result

--- 5-rock-paper-scissors/app.js
+++ 5-rock-paper-scissors/app.js
@@ -5,6 +5,10 @@ const SCISSORS = 'SCISSORS';
 const PAPER = 'PAPER';
 const DEFAULT_USER_CHOICE = ROCK;
 
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
@@ -17,13 +21,40 @@ const getPlayerChoice = () => {
   return DEFAULT_USER_CHOICE
 }
 
+const getComputedChoice = () => {
+  const randomValue = Math.random();
+
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue > 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+}
+
+const getResult = (pChoice, cChoice) => {
+  if (pChoice === cChoice) {
+    return RESULT_DRAW;
+  } else if (pChoice === ROCK && cChoice === SCISSORS
+  || pChoice === SCISSORS && cChoice === PAPER
+  || pChoice === PAPER && cChoice === ROCK) {  // win player
+    return RESULT_PLAYER_WINS;
+  } else {  // win computer
+    return RESULT_COMPUTER_WINS;
+  }
+}
+
 const startGame = () => {
   if (gameIsRunning) {
     return ;
   }
   console.log('game is starting');
   gameIsRunning = true;
-  getPlayerChoice();
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputedChoice();
+  const winner = getResult(playerChoice, computerChoice)
+  console.log(winner);
 }
 
 startGameBtn.addEventListener('click', startGame);
