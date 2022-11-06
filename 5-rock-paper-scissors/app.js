c6651aa4bc966b230076d572e1d0c21740016141
강명구 <mg5566@hanmail.net>
2022-11-06 14:52:17 +0900

    feat: add game start and select choice feature

--- 5-rock-paper-scissors/app.js
+++ 5-rock-paper-scissors/app.js
@@ -1,2 +1,29 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const SCISSORS = 'SCISSORS';
+const PAPER = 'PAPER';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(`${ROCK} ${SCISSORS} or ${PAPER}`).toUpperCase();
+
+  if (selection !== ROCK && selection !== SCISSORS && selection !== PAPER) {
+    alert(`invalid choice! We chose ${DEFAULT_USER_CHOICE} for you`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return DEFAULT_USER_CHOICE
+}
+
+const startGame = () => {
+  if (gameIsRunning) {
+    return ;
+  }
+  console.log('game is starting');
+  gameIsRunning = true;
+  getPlayerChoice();
+}
+
+startGameBtn.addEventListener('click', startGame);
