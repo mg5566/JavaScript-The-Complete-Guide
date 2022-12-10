a41e355729740c2dde241292457778f882c8826d
강명구 <mg5566@hanmail.net>
2022-12-10 18:57:44 +0900

    todays learn

--- 7-DOM/app.js
+++ 7-DOM/app.js
@@ -1,3 +1,4 @@
+/*
 window.alert(`this is window's alert`);
 
 const alert = () => {
@@ -5,4 +6,18 @@ const alert = () => {
 }
 
 alert();
+*/
 
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  /*
+  if (section.className === 'red-bg vis') {
+    section.className = 'invis';
+  } else {
+    section.className = 'red-bg vis';
+  }
+  */
+  section.classList.toggle('vis');
+})
