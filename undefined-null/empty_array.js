17bde86dbda6c35a3d8832a7d536b878e7a70771
mg5566 <mg5566@hanmail.net>
2023-05-01 15:25:29 +0900

    빈 요소와 배열의 순회

---
+++ undefined-null/empty_array.js
@@ -0,0 +1,53 @@
+function print_array(arr, msg = '') {
+  console.log('---' + msg + '---------------');
+  arr.forEach(function (item, index) {
+    console.log(item, index);
+  });
+  console.log('------------------');
+};
+
+var arr1 = [undefined, 1];
+var arr2 = [];
+arr2[1] = 1;
+
+/*
+arr1.forEach(function (item, index) {
+  console.log(item, index);
+});  // undefined 0 / 1 1
+arr2.forEach(function (item, index) {
+  console.log(item, index);
+});  // 1 1
+*/
+print_array(arr1);  // undefined 0 / 1 1
+print_array(arr2);  // 1 1
+
+/*
+arr1.map(function (item, index) {
+  return item + index;
+});  // [NaN, 2]
+arr2.map(function (item, index) {
+  return item + index;
+});  // [empty, 2]
+*/
+print_array(arr1.map(function (item, index) {
+  return item + index;
+}), 'arr1 map');  // [NaN, 2]
+print_array(arr2.map(function (item, index) {
+  return item + index;
+}), 'arr2 map');  // [empty, 2]
+
+print_array(arr1.filter(function (item) {
+  return !item;
+}), 'arr1 filter');  // [undefined 0]
+print_array(arr2.filter(function (item) {
+  return !item;
+}), 'arr2 filter');  // []
+
+arr1.reduce(function (prev, cur, index) {
+  return prev + cur + index;
+}, '');
+arr2.reduce(function (prev, cur, index) {
+  return prev + cur + index;
+}, '');
+print_array(arr1, 'arr1 reduce');  // undefined 0 / 1 1
+print_array(arr2, 'arr2 reduce');  // 1 1
